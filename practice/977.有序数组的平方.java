77fc28b68e4b172535a50bc0546c530a0cc913f4
Abigail-0821 <abigail8n21@gmail.com>
2022-09-20 00:21:21 -0700

    9/19 slow fast pointer

--- practice/977.有序数组的平方.java
+++ practice/977.有序数组的平方.java
@@ -8,24 +8,44 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
         int i = 0;
-        int k = nums.length - 1;
+        int j = nums.length - 1;
 
-        int pos = nums.length - 1;
+        int k = nums.length - 1;
 
         int[] result = new int[nums.length];
 
-        while (i <= k) {
-            if (nums[i] * nums[i] < nums[k] * nums[k]) {
-                result[pos] = nums[k] * nums[k];
-                k--;
-            } else {
-                result[pos] = nums[i] * nums[i];
+        while (i <= j) {
+            if (Math.abs(nums[i]) > Math.abs(nums[j])) {
+                result[k] = nums[i] * nums[i];
                 i++;
+            } else {
+                result[k] = nums[j] * nums[j];
+                j--;
             }
-            pos--;
+            k--;
         }
 
         return result;
+
+        // int i = 0;
+        // int k = nums.length - 1;
+
+        // int pos = nums.length - 1;
+
+        // int[] result = new int[nums.length];
+
+        // while (i <= k) {
+        // if (nums[i] * nums[i] < nums[k] * nums[k]) {
+        // result[pos] = nums[k] * nums[k];
+        // k--;
+        // } else {
+        // result[pos] = nums[i] * nums[i];
+        // i++;
+        // }
+        // pos--;
+        // }
+
+        // return result;
     }
 }
 // @lc code=end
