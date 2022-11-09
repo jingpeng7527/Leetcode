71564ec279b9e6ae56773820128352162c55c093
Abigail-0821 <abigail8n21@gmail.com>
2022-11-09 02:01:36 -0800

    hash table

---
+++ practice/242.valid-anagram.java
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=242 lang=java
+ *
+ * [242] Valid Anagram
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] count = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < t.length(); i++) {
+            count[t.charAt(i) - 'a']--;
+        }
+
+        for (int i : count) {
+            if (i != 0) {
+                return false;
+            }
+        }
+        return true;
+
+    }
+}
+// @lc code=end
