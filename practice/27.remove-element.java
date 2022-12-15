2d43a936784e12109bae2024d7f326222b3acd11
Abigail-0821 <abigail8n21@gmail.com>
2022-12-15 01:02:01 -0800

    2022.12.14 double pointer

---
+++ practice/27.remove-element.java
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=27 lang=java
+ *
+ * [27] Remove Element
+ */
+
+// @lc code=start
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int slow = 0;
+        for (int fast = 0; fast < nums.length; fast++) {
+            if (nums[fast] != val) {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+        }
+        return slow;
+    }
+}
+// @lc code=end
