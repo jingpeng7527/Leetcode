513ed30c78dcd42675df8fca9ab9466e5b09890b
Abigail-0821 <abigail8n21@gmail.com>
2022-12-15 01:20:29 -0800

    2022.12.14 double pointer

--- practice/27.remove-element.java
+++ practice/27.remove-element.java
@@ -7,14 +7,31 @@
 // @lc code=start
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int slow = 0;
-        for (int fast = 0; fast < nums.length; fast++) {
-            if (nums[fast] != val) {
-                nums[slow] = nums[fast];
-                slow++;
-            }
+        // int slow = 0;
+        // for (int fast = 0; fast < nums.length; fast++) {
+        // if (nums[fast] != val) {
+        // nums[slow] = nums[fast];
+        // slow++;
+        // }
+        // }
+        // return slow;
+
+        int left = 0;
+        int right = nums.length - 1;
+        while (right >= 0 && nums[right] == val) {
+            right--;
         }
-        return slow;
+        while (left <= right) {
+
+            if (nums[left] == val) {
+                nums[left] = nums[right];
+                right--;
+            }
+            left++;
+            while(right >= 0 && nums[right] == val) right--;
+        }
+
+        return left;
     }
 }
 // @lc code=end
