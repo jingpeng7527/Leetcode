19ba0e08004a0cc67d49e4a1392a1947abaf6586
Abigail-0821 <abigail8n21@gmail.com>
2023-02-23 15:53:20 -0800

    2023.2.23 binary tree

--- practice/116.populating-next-right-pointers-in-each-node.java
+++ practice/116.populating-next-right-pointers-in-each-node.java
@@ -1,3 +1,7 @@
+import java.util.Queue;
+
+import org.w3c.dom.Node;
+
 /*
  * @lc app=leetcode id=116 lang=java
  *
@@ -30,7 +34,46 @@ class Node {
 
 class Solution {
     public Node connect(Node root) {
-        
+        if (root == null) {
+            return null;
+        }
+
+        Node now = root;
+
+        while (now.left!=null) {
+            Node pointer = now;
+
+            while (pointer != null) {
+                pointer.left.next = pointer.right;
+                if (pointer.next!=null) {
+                    pointer.right.next = pointer.next.left;
+                }
+                pointer = pointer.next;
+            }
+            now = now.left;
+        }
+
+        // Queue<Node> nodes = new LinkedList<>();
+        // nodes.add(root);
+        // int len = nodes.size();
+
+        // while (!nodes.isEmpty()) {
+        //     len = nodes.size();
+        //     while (len > 0) {
+        //         Node now = nodes.poll();
+        //         if (len > 1) {
+        //             now.next = nodes.peek();
+        //         }
+                
+        //         if (now.left != null)
+        //             nodes.offer(now.left);
+        //         if (now.right != null)
+        //             nodes.offer(now.right);
+        //         len--;
+        //     }
+
+        // }
+        return root;
     }
 }
 // @lc code=end
