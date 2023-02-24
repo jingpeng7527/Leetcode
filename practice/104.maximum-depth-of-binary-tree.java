7710cb990a7372d325de9e8471d7d94a138ee3ca
Abigail-0821 <abigail8n21@gmail.com>
2023-02-23 16:21:52 -0800

    2023.2.23 binary tree

---
+++ practice/104.maximum-depth-of-binary-tree.java
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=104 lang=java
+ *
+ * [104] Maximum Depth of Binary Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } else {
+            int maxLeft = maxDepth(root.left);
+            int maxRight = maxDepth(root.right);
+            return Math.max(maxLeft, maxRight) + 1;
+        }
+    }
+}
+// @lc code=end
+
