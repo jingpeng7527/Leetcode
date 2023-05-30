9250bac4bb088158bb90c83dc2c0c0dd526767e2
Abigail-0821 <abigail8n21@gmail.com>
2023-05-30 15:46:35 -0700

    2023.5.30 pointer

---
+++ practice/88.merge-sorted-array.java
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=88 lang=java
+ *
+ * [88] Merge Sorted Array
+ */
+
+// @lc code=start
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m-1;
+        int j = n-1;
+        int y = m+n-1;
+        while (i >= 0 && j >= 0) {
+            nums1[y--] = nums1[i] > nums2[j] ? nums1[i--] : nums2[j--];
+            // if (nums1[i] > nums2[j]) {
+            //     nums1[y] = nums1[i];
+            //     i--;
+            // } else {
+            //     nums1[y] = nums2[j];
+            //     j--;
+            // }
+            // y--;
+        }
+        System.arraycopy(nums2, 0, nums1, 0, j + 1);
+    }
+}
+// @lc code=end
+
