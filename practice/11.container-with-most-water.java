58d65a1f5e11f4ae8145a68ad86efbdf32eb4458
Abigail-0821 <abigail8n21@gmail.com>
2023-05-31 06:58:09 -0700

    2023.5.30 bi-pointer

---
+++ practice/11.container-with-most-water.java
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=11 lang=java
+ *
+ * [11] Container With Most Water
+ */
+
+// @lc code=start
+class Solution {
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length-1;
+        int res = 0;
+        
+        while (left < right) {
+            int area = (right - left) * Math.min(height[left], height[right]);
+            if (height[left]<height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+            res = Math.max(res, area);
+        }
+        
+        return res;
+    }
+}
+// @lc code=end
+
