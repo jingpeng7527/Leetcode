20b706b88e1cc7e2ced640697da2d573bbc9d9cd
Abigail-0821 <abigail8n21@gmail.com>
2023-07-12 18:36:16 -0700

    2023.7.12 单调stack解法

--- practice/496.next-greater-element-i.java
+++ practice/496.next-greater-element-i.java
@@ -6,37 +6,51 @@
 
 // @lc code=start
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Stack;
 
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        Stack<Integer> stack = new Stack<>();
-        Stack<Integer> tmp = new Stack<>();
-        
+
         int[] res = new int[nums1.length];
 
-        for (int i : nums2) {
-            stack.add(i);
-            // tmp.add(i);
-        }
+        Deque<Integer> stack = new ArrayDeque<>();
+        Map<Integer, Integer> map = new HashMap<>();
 
-        for (int i = 0; i < nums1.length; i++) { 
-            tmp.addAll(stack);
-            int max = -1;
-            // System.out.println(tmp);
-            while (!tmp.isEmpty()) {
-                int cur = tmp.pop();
-                if (cur > nums1[i]) {
-                    max = cur;
-                } else if (cur == nums1[i]) {
-                    break;
-                }
-                // System.out.println(max);
+        for (int j = nums2.length - 1; j >= 0; j--) {
+            while (!stack.isEmpty() && nums2[j] >= stack.peek()) {
+                stack.pop();
             }
-            res[i] = max;
-            // tmp = stack;
+            map.put(nums2[j], stack.isEmpty() ? -1 : stack.peek());
+            stack.push(nums2[j]);
         }
-
+        
+        for (int i = 0; i < nums1.length; i++) {
+            res[i] = map.get(nums1[i]);
+        }
+                
+        // Stack<Integer> stack = new Stack<>();
+        // Stack<Integer> tmp = new Stack<>();
+        
+        // for (int i : nums2) {
+        //     stack.add(i);
+        // }
+        // for (int i = 0; i < nums1.length; i++) { 
+        //     tmp.addAll(stack);
+        //     int max = -1;
+        //     while (!tmp.isEmpty()) {
+        //         int cur = tmp.pop();
+        //         if (cur > nums1[i]) {
+        //             max = cur;
+        //         } else if (cur == nums1[i]) {
+        //             break;
+        //         }
+        //     }
+        //     res[i] = max;
+        // }
         return res;
     }
 }
