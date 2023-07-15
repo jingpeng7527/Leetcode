58c2f6cb25d71b1c6fb3e452f6b3c20c8e1ee5e8
Abigail-0821 <abigail8n21@gmail.com>
2023-07-14 18:58:15 -0700

    2023.7.14 hashmap

---
+++ practice/705.design-hash-set.java
@@ -0,0 +1,73 @@
+/*
+ * @lc app=leetcode id=705 lang=java
+ *
+ * [705] Design HashSet
+ */
+
+// @lc code=start
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+class MyHashSet {
+    private static final int BASE = 769;
+    private LinkedList[] data;
+
+    public MyHashSet() {
+        data = new LinkedList[BASE];
+        for (int i = 0; i < BASE; ++i) {
+            data[i] = new LinkedList<Integer>();
+        }
+    }
+    
+    public void add(int key) {
+        int h = hash(key);
+        Iterator<Integer> iterator = data[h].iterator();
+        while (iterator.hasNext()) {
+            Integer element = iterator.next();
+            if (element == key) {
+                return;
+            }
+        }
+        data[h].offerLast(key);
+    }
+    
+    public void remove(int key) {
+        int h = hash(key);
+        Iterator<Integer> iterator = data[h].iterator();
+        while (iterator.hasNext()) {
+            Integer element = iterator.next();
+            if (element == key) {
+                data[h].remove(element);
+                return;
+            }
+        }
+        // data[h].offerLast(key);
+    }
+    
+    public boolean contains(int key) {
+        int h = hash(key);
+        Iterator<Integer> iterator = data[h].iterator();
+        while (iterator.hasNext()) {
+            Integer element = iterator.next();
+            if (element == key) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static int hash(int key) {
+        return key % BASE;
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
+// @lc code=end
+
