4b5e6647454846354d920423e694e253c63d3522
Abigail-0821 <abigail8n21@gmail.com>
2023-08-02 18:36:12 -0700

    2023.8.2 sliding window

--- practice/209.minimum-size-subarray-sum.java
+++ practice/209.minimum-size-subarray-sum.java
@@ -7,23 +7,35 @@
 // @lc code=start
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
-        if (nums.length == 0) {
-            return 0;
-        }
-        int left = 0, right = 0;
+        int res = Integer.MAX_VALUE;
         int sum = 0;
-        int res = nums.length + 1;
-        while (right < nums.length) {
-            sum += nums[right];
+        int left = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
             while (sum >= target) {
-                res = Math.min(right - left+1, res);
-                sum -= nums[left];
-                left++;
+                res = Math.min(res, i - left + 1);
+                sum -= nums[left++];
             }
-            right++;
         }
+        return res == Integer.MAX_VALUE ? 0 : res;
 
-        return res == nums.length + 1 ? 0 : res;
+        // if (nums.length == 0) {
+        //     return 0;
+        // }
+        // int left = 0, right = 0;
+        // int sum = 0;
+        // int res = nums.length + 1;
+        // while (right < nums.length) {
+        //     sum += nums[right];
+        //     while (sum >= target) {
+        //         res = Math.min(right - left+1, res);
+        //         sum -= nums[left];
+        //         left++;
+        //     }
+        //     right++;
+        // }
+
+        // return res == nums.length + 1 ? 0 : res;
     }
 }
 // @lc code=end
