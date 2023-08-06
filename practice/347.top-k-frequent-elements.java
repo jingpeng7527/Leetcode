885e325299dbd6eb5f141b125bf76c5d4d675d8c
Abigail-0821 <abigail8n21@gmail.com>
2023-08-06 01:01:23 -0700

    2023.8.5 review hashing - neetcode75

--- practice/347.top-k-frequent-elements.java
+++ practice/347.top-k-frequent-elements.java
@@ -10,39 +10,53 @@ import java.util.*;
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         Map<Integer, Integer> map = new HashMap<>();
+        int[] res = new int[k];
         for (int i : nums) {
             map.put(i, map.getOrDefault(i, 0) + 1);
         }
 
         PriorityQueue<int[]> pq = new PriorityQueue<>((pair1, pair2) -> pair1[1] - pair2[1]);
+
         for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
             if (pq.size() < k) {
                 pq.add(new int[] { entry.getKey(), entry.getValue() });
             } else {
-                if (pq.peek()[1] < entry.getValue()) {
+                if (entry.getValue() > pq.peek()[1]) {
                     pq.poll();
                     pq.add(new int[] { entry.getKey(), entry.getValue() });
                 }
             }
         }
 
-        // PriorityQueue<int[]> pq = new PriorityQueue<>((pair1, pair2) -> pair2[1] -
-        // pair1[1]);
-        // for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-        // pq.add(new int[] { entry.getKey(), entry.getValue() });
-        // }
-
-        int[] ans = new int[k];
-        for (int i = k-1; i >= 0; i--) {
-            ans[i] = pq.poll()[0];
+        for (int i = 0; i < k; i++) {
+            res[i] = pq.poll()[0];
         }
 
-        // int[] ans = new int[k];
-        // for(int i=0;i<k;i++){//依次从队头弹出k个,就是出现频率前k高的元素
-        // ans[i] = pq.poll()[0];
+        return res;
+        
+        // Map<Integer, Integer> map = new HashMap<>();
+        // for (int i : nums) {
+        //     map.put(i, map.getOrDefault(i, 0) + 1);
         // }
 
-        return ans;
+        // PriorityQueue<int[]> pq = new PriorityQueue<>((pair1, pair2) -> pair1[1] - pair2[1]);
+        // for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+        //     if (pq.size() < k) {
+        //         pq.add(new int[] { entry.getKey(), entry.getValue() });
+        //     } else {
+        //         if (pq.peek()[1] < entry.getValue()) {
+        //             pq.poll();
+        //             pq.add(new int[] { entry.getKey(), entry.getValue() });
+        //         }
+        //     }
+        // }
+
+        // int[] ans = new int[k];
+        // for (int i = 0; i < k; i++) {// 依次从队头弹出k个,就是出现频率前k高的元素
+        //     ans[i] = pq.poll()[0];
+        // }
+
+        // return ans;
     }
 }
 // @lc code=end
