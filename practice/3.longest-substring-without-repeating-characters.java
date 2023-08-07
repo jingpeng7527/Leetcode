d2c6694b22ac3e1d7451f58f91aef34827266345
Abigail-0821 <abigail8n21@gmail.com>
2023-08-07 15:53:26 -0700

    2023.8.7 sliding window - neetcode75

--- practice/3.longest-substring-without-repeating-characters.java
+++ practice/3.longest-substring-without-repeating-characters.java
@@ -13,17 +13,34 @@ class Solution {
         if (s.length() == 0) {
             return 0;
         }
+
+        Map<Character, Integer> map = new HashMap<>();
+
         int left = 0;
+        int right = 0;
         int max = 0;
-        HashMap<Character, Integer> map = new HashMap<>();
-        for (int i = 0; i < s.length(); i++) {
-            if (map.containsKey(s.charAt(i))) {
-                left = Math.max(left, map.get(s.charAt(i)) + 1);
+
+        while (right<s.length()) {
+            if (map.containsKey(s.charAt(right))) {
+                left = Math.max(left, map.get(s.charAt(right))+1);
             }
-            map.put(s.charAt(i), i);
-            max = Math.max(max, i-left+1);
+            map.put(s.charAt(right), right);
+            max = Math.max(max, right-left+1);
+            right++;
         }
+            
         return max;
+        // int left = 0;
+        // int max = 0;
+        // HashMap<Character, Integer> map = new HashMap<>();
+        // for (int i = 0; i < s.length(); i++) {
+        //     if (map.containsKey(s.charAt(i))) {
+        //         left = Math.max(left, map.get(s.charAt(i)) + 1);
+        //     }
+        //     map.put(s.charAt(i), i);
+        //     max = Math.max(max, i-left+1);
+        // }
+        // return max;
     }
 }
 // @lc code=end
