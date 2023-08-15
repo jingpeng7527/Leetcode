f14cc79f2cec7140d98d077fc729e6356bc3fc85
Abigail-0821 <abigail8n21@gmail.com>
2023-08-15 14:25:23 -0700

    2023.8.15 backtracking

--- practice/78.subsets.java
+++ practice/78.subsets.java
@@ -7,33 +7,46 @@
 // @lc code=start
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 class Solution {
+    List<List<Integer>> res = new ArrayList<>();
     public List<List<Integer>> subsets(int[] nums) {
         // backtracking
-        List<List<Integer>> res = new ArrayList<>();
-        List<Integer> str = new ArrayList<>();
-        // res.add(new ArrayList<>());
-        for (int i = 0; i <= nums.length; i++) {
-            backTracking(nums, res, i, 0, str);
-        }
+        List<Integer> tmp = new ArrayList<>();
+        backTracking(0, tmp, nums);
         return res;
     }
 
-    public void backTracking(int[] nums, List<List<Integer>> res, int length, int start, List<Integer> str) {
-        if (str.size() == length) {
-            List<Integer> tmp = new ArrayList<>(str);
-            res.add(tmp);
-            return;
-        }
-
+    public void backTracking(int start, List<Integer> tmp, int[] nums ) 
+    {
+        res.add(new ArrayList<>(tmp));
         for (int i = start; i < nums.length; i++) {
-            str.add(nums[i]);
-            backTracking(nums, res, length, i + 1, str);
-            str.remove(str.size() - 1);
+            tmp.add(nums[i]);
+            backTracking(i+1, tmp, nums);
+            tmp.remove(tmp.size() - 1);
         }
+        
+    //     List<List<Integer>> res = new ArrayList<>();
+    //     List<Integer> str = new ArrayList<>();
+    //     for (int i = 0; i <= nums.length; i++) {
+    //         backTracking(nums, res, i, 0, str);
+    //     }
+    //     return res;
+    // }
+
+    // public void backTracking(int[] nums, List<List<Integer>> res, int length, int start, List<Integer> str) {
+    //     if (str.size() == length) {
+    //         List<Integer> tmp = new ArrayList<>(str);
+    //         res.add(tmp);
+    //         return;
+    //     }
+
+    //     for (int i = start; i < nums.length; i++) {
+    //         str.add(nums[i]);
+    //         backTracking(nums, res, length, i + 1, str);
+    //         str.remove(str.size() - 1);
+    //     }
 
         // //扩展法
         // List<List<Integer>> list = new ArrayList<>();
