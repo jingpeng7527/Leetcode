7a09149ebaa5c90e6f19aff89c763813d28f7659
Abigail-0821 <abigail8n21@gmail.com>
2023-08-15 12:35:25 -0700

    2023.8.15 backtracking

--- practice/22.generate-parentheses.java
+++ practice/22.generate-parentheses.java
@@ -12,28 +12,48 @@ import java.util.List;
 class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> res = new ArrayList<>();
-        backTracking(n, res, 0, 0, "");
+        StringBuilder sb = new StringBuilder();
+        backTracking(n, n, sb, res);
         return res;
     }
 
-    public void backTracking(int n, List<String> res, int left, int right, String str) {
-        if (right > left) {
+    public void backTracking(int left, int right, StringBuilder sb, List<String> res) {
+        if (left < 0 || right < 0 || left > right)
             return;
-        }
-        if (left == right && left == n) {
-            res.add(str);
+        
+        if (left == 0 && right == 0) {
+            res.add(sb.toString());
             return;
         }
         
-        if (left < n) {
-            backTracking(n, res, left + 1, right, str + "(");
-        }
-        
-        if (right < left) {
-            backTracking(n, res, left, right+1, str+")");
-        }
+        sb.append("(");// 选择
+        backTracking(left - 1, right, sb, res);
+        sb.deleteCharAt(sb.length() - 1);// 撤消选择
 
+        sb.append(")");
+        backTracking(left, right-1, sb, res);
+        sb.deleteCharAt(sb.length() - 1);
     }
+
+
+    // public void backTracking(int n, List<String> res, int left, int right, String str) {
+    //     if (right > left) {
+    //         return;
+    //     }
+    //     if (left == right && left == n) {
+    //         res.add(str);
+    //         return;
+    //     }
+        
+    //     if (left < n) {
+    //         backTracking(n, res, left + 1, right, str + "(");
+    //     }
+        
+    //     if (right < left) {
+    //         backTracking(n, res, left, right+1, str+")");
+    //     }
+
+    // }
 }
 // @lc code=end
 
