159d86573df11dd2f229d41c413ace3163dda5f8
Abigail-0821 <abigail8n21@gmail.com>
2023-09-02 15:41:32 -0700

    2023.9.2 hash - review

--- practice/49.group-anagrams.java
+++ practice/49.group-anagrams.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -12,34 +12,54 @@ import java.util.Map;
 // @lc code=start
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String, List<String>> map = new HashMap<String, List<String>>();
-        for (String string : strs) {
-            // char[] c = string.toCharArray();
-            // Arrays.sort(c);
-            // String key = new String(c);
 
+        Map<String, List<String>> map = new HashMap<>();
+        for (String string : strs) {
             int[] counts = new int[26];
-            int length = string.length();
-            for (int i = 0; i < length; i++) {
-                counts[string.charAt(i) - 'a']++;
+            for (char c : string.toCharArray()) {
+                counts[c - 'a']++;
             }
-            // 将每个出现次数大于 0 的字母和出现次数按顺序拼接成字符串，作为哈希表的键
+
             StringBuffer sb = new StringBuffer();
             for (int i = 0; i < 26; i++) {
                 if (counts[i] != 0) {
-                    sb.append((char) ('a' + i));
+                    sb.append(i + 'a');
                     sb.append(counts[i]);
                 }
-                // System.out.println(sb.toString());
             }
             String key = sb.toString();
-            // System.out.println(key);
-
-            List<String> list = map.getOrDefault(key, new ArrayList<String>());
-            list.add(string);
-            map.put(key, list);
+            List<String> llist = map.getOrDefault(key, new ArrayList<>());
+            llist.add(string);
+            map.put(key, llist);
         }
         return new ArrayList<List<String>>(map.values());
+
+
+        // Map<String, List<String>> map = new HashMap<String, List<String>>();
+        // for (String string : strs) {
+
+        //     int[] counts = new int[26];
+        //     int length = string.length();
+        //     for (int i = 0; i < length; i++) {
+        //         counts[string.charAt(i) - 'a']++;
+        //     }
+        //     // 将每个出现次数大于 0 的字母和出现次数按顺序拼接成字符串，作为哈希表的键
+        //     StringBuffer sb = new StringBuffer();
+        //     for (int i = 0; i < 26; i++) {
+        //         if (counts[i] != 0) {
+        //             sb.append((char) ('a' + i));
+        //             sb.append(counts[i]);
+        //         }
+        //         // System.out.println(sb.toString());
+        //     }
+        //     String key = sb.toString();
+        //     // System.out.println(key);
+
+        //     List<String> list = map.getOrDefault(key, new ArrayList<String>());
+        //     list.add(string);
+        //     map.put(key, list);
+        // }
+        // return new ArrayList<List<String>>(map.values());
     }
 }
 // @lc code=end
