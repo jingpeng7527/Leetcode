e47084a05f442b03fb76479bab606bba2d24d800
Abigail-0821 <abigail8n21@gmail.com>
2023-09-02 15:41:49 -0700

    2023.9.1 bfs - review

--- practice/102.binary-tree-level-order-traversal.java
+++ practice/102.binary-tree-level-order-traversal.java
@@ -1,4 +1,6 @@
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
@@ -35,43 +37,71 @@ class Solution {
             return result;
         }
 
-    public void checkFun01(TreeNode node, Integer deep) {
-        if (node == null) {
-            return;
-        }
-        deep++;
-        if (result.size()<deep) {
-            result.add(new ArrayList<Integer>());
-        }       
-        result.get(deep-1).add(node.val);
-        checkFun01(node.left, deep);
-        checkFun01(node.right, deep);
-    }
-        
-    public void checkFun02(TreeNode node) {
-        if (node == null) {
-            return;
-        }
-        Queue<TreeNode> que = new LinkedList<TreeNode>();
-        que.offer(node);
-
-        while (!que.isEmpty()) {
-            
-            List<Integer> list = new ArrayList<Integer>();
-            int len = que.size();
-            while (len > 0) {
-                node = que.poll();
-                list.add(node.val);
-                if (node.left != null) {
-                    que.offer(node.left);
-                }
-                if (node.right != null) {
-                    que.offer(node.right);
-                }
-                len--;
+        // public void checkFun01(TreeNode node, Integer deep) {
+        //     if (node == null) {
+        //         return;
+        //     }
+        //     deep++;
+        //     if (result.size() < deep) {
+        //         result.add(new ArrayList<Integer>());
+        //     }
+        //     result.get(deep - 1).add(node.val);
+        //     checkFun01(node.left, deep);
+        //     checkFun01(node.right, deep);
+        // }
+    
+        public void checkFun02(TreeNode node) {
+            if (node == null) {
+                return;
             }
-            result.add(list);
+            Deque<TreeNode> dq = new ArrayDeque<>();
+            dq.add(node);
+
+            while (!dq.isEmpty()) {
+                int sz = dq.size();
+                // System.out.println(sz);
+                List<Integer> list = new ArrayList<>();
+                for (int i = 0; i < sz; i++) {
+                    TreeNode cur = dq.poll();
+                    list.add(cur.val);
+                    if (cur.left != null) {
+                        dq.add(cur.left);
+                    }
+                    if (cur.right != null) {
+                        dq.add(cur.right);
+                    }
+                }
+
+                result.add(list);
+
+            }
+
         }
-    }
+        
+    // public void checkFun02(TreeNode node) {
+    //     if (node == null) {
+    //         return;
+    //     }
+    //     Queue<TreeNode> que = new LinkedList<TreeNode>();
+    //     que.offer(node);
+
+    //     while (!que.isEmpty()) {
+            
+    //         List<Integer> list = new ArrayList<Integer>();
+    //         int len = que.size();
+    //         while (len > 0) {
+    //             node = que.poll();
+    //             list.add(node.val);
+    //             if (node.left != null) {
+    //                 que.offer(node.left);
+    //             }
+    //             if (node.right != null) {
+    //                 que.offer(node.right);
+    //             }
+    //             len--;
+    //         }
+    //         result.add(list);
+    //     }
+    // }
 }
 // @lc code=end
--- practice/111.minimum-depth-of-binary-tree.java
+++ practice/111.minimum-depth-of-binary-tree.java
@@ -5,6 +5,9 @@
  */
 
 // @lc code=start
+
+import java.util.Queue;
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -29,26 +32,46 @@ class Solution {
         
         Queue<TreeNode> queue = new LinkedList<>();
 
-        queue.offer(root);
-        int depth = 0;
+        queue.add(root);
+        int depth = 1;
 
         while (!queue.isEmpty()) {
-            int size = queue.size();
-            while (size != 0) {
+            int sz = queue.size();
+            for (int i = 0; i < sz; i++) {
                 TreeNode cur = queue.poll();
-                if (cur.left == null && cur.right == null) {
-                    return depth+1;
+                if (cur.left == null & cur.right == null) {
+                    return depth;
                 }
                 if (cur.left != null) {
-                    queue.offer(cur.left);
+                    queue.add(cur.left);
                 }
                 if (cur.right != null) {
-                    queue.offer(cur.right);
+                    queue.add(cur.right);
                 }
-                size--;
             }
             depth++;
         }
+
+        // queue.offer(root);
+        // int depth = 0;
+
+        // while (!queue.isEmpty()) {
+        //     int size = queue.size();
+        //     while (size != 0) {
+        //         TreeNode cur = queue.poll();
+        //         if (cur.left == null && cur.right == null) {
+        //             return depth+1;
+        //         }
+        //         if (cur.left != null) {
+        //             queue.offer(cur.left);
+        //         }
+        //         if (cur.right != null) {
+        //             queue.offer(cur.right);
+        //         }
+        //         size--;
+        //     }
+        //     depth++;
+        // }
 
         return depth;
 
