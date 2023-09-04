9752f97050afd430e8de694c90ad4895bc0347be
Abigail-0821 <abigail8n21@gmail.com>
2023-09-04 16:51:01 -0700

    2023.9.4 linkedlist

---
+++ practice/86.partition-list.java
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode id=86 lang=java
+ *
+ * [86] Partition List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+        ListNode dummy1 = new ListNode(-1);
+        ListNode dummy2 = new ListNode(-1);
+        ListNode p1 = dummy1, p2 = dummy2;
+
+        while (head != null) {
+            if (head.val < x) {
+                p1.next = head;
+                p1 = p1.next;
+            } else {
+                p2.next = head;
+                p2 = p2.next;
+            }
+            head = head.next;
+        }
+        
+        p1.next = dummy2.next;
+        p2.next = null;
+        return dummy1.next;
+    }
+}
+// @lc code=end
+
