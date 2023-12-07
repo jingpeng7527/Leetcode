9ab9f8cf0dcfac041e256291581fb4e8fee954ed
Abigail-0821 <abigail8n21@gmail.com>
2023-12-07 02:21:48 -0800

    2023.12.7 dfs-review

--- practice/104.maximum-depth-of-binary-tree.java
+++ practice/104.maximum-depth-of-binary-tree.java
@@ -30,28 +30,31 @@ class Solution {
         if (root == null) {
             return 0;
         }
+
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
         
-        Queue<TreeNode> queue = new LinkedList<>();
+        // Queue<TreeNode> queue = new LinkedList<>();
 
-        queue.offer(root);
-        int depth = 0;
+        // queue.offer(root);
+        // int depth = 0;
 
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            while (size != 0) {
-                TreeNode cur = queue.poll();
-                if (cur.left != null) {
-                    queue.offer(cur.left);
-                }
-                if (cur.right != null) {
-                    queue.offer(cur.right);
-                }
-                size--;
-            }
-            depth++;
-        }
+        // while (!queue.isEmpty()) {
+        //     int size = queue.size();
+        //     while (size != 0) {
+        //         TreeNode cur = queue.poll();
+        //         if (cur.left != null) {
+        //             queue.offer(cur.left);
+        //         }
+        //         if (cur.right != null) {
+        //             queue.offer(cur.right);
+        //         }
+        //         size--;
+        //     }
+        //     depth++;
+        // }
 
-        return depth;
+        // return depth;
+
         // else {
         //     int maxLeft = maxDepth(root.left);
         //     int maxRight = maxDepth(root.right);
