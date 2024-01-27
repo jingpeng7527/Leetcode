5117de3142b0cf140053aa483458d3e0a296319d
Abigail-0821 <abigail8n21@gmail.com>
2024-01-26 17:37:41 -0800

    2024.1.26 using stringbuilder as stack

--- practice/1047.remove-all-adjacent-duplicates-in-string.java
+++ practice/1047.remove-all-adjacent-duplicates-in-string.java
@@ -9,25 +9,44 @@ import java.util.*;
 // @lc code=start
 class Solution {
     public String removeDuplicates(String s) {
-        Deque<Character> stcak = new LinkedList<>();
-        stcak.push(s.charAt(0));
+        StringBuilder sb = new StringBuilder();
 
-        for (int i = 1; i < s.length(); i++) {
-
-            if ( !stcak.isEmpty() && s.charAt(i) == stcak.peek()) {
-                stcak.pop();
+        for (int i = 0; i < s.length(); i++) {
+            if (sb.length() != 0 && s.charAt(i) == sb.charAt(sb.length() - 1)) {
+                sb.deleteCharAt(sb.length() - 1);
             } else {
-                stcak.push(s.charAt(i));
+                sb.append(s.charAt(i));
             }
         }
 
-        String str = "";
-    
-        while (!stcak.isEmpty()) {
-            str = stcak.pop() + str;
-        }
+        return sb.toString();
 
-        return str;
+
+        // Deque<Character> stack = new LinkedList<>();
+        // stack.push(s.charAt(0));
+
+        // for (int i = 1; i < s.length(); i++) {
+
+        //     if ( !stack.isEmpty() && s.charAt(i) == stack.peek()) {
+        //         stack.pop();
+        //     } else {
+        //         stack.push(s.charAt(i));
+        //     }
+        // }
+
+        // // String str = "";
+    
+        // // while (!stack.isEmpty()) {
+        // //     str = stcak.pop() + str;
+        // // }
+
+        // // return str;
+
+        // StringBuilder sb = new StringBuilder("");
+        // while (!stack.isEmpty()) {
+        //     sb.append(stack.pop());
+        // }
+        // return sb.reverse().toString();
     }
 }
 // @lc code=end
