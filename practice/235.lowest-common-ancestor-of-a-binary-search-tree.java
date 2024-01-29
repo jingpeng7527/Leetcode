3172514bdc01040c64d0cda5738d1eddd19437ce
jingpeng7527 <pengjing7527@163.com>
2024-01-29 12:20:54 -0800

    2024.1.29 BSF

---
+++ practice/235.lowest-common-ancestor-of-a-binary-search-tree.java
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode id=235 lang=java
+ *
+ * [235] Lowest Common Ancestor of a Binary Search Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        TreeNode cur = root;
+        while (cur != null) {
+            if (p.val < cur.val && q.val < cur.val) {
+                cur = cur.left;
+            } else if (p.val > cur.val && q.val > cur.val) {
+                cur = cur.right;
+            } else {
+                return cur;
+            }
+        }
+        return root;
+    }
+}
+// @lc code=end
+
