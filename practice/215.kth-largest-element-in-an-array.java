ee320372879d92197669a77134fa3fde3a7d4595
jingpeng7527 <pengjing7527@163.com>
2024-02-10 19:46:49 -0800

    2024.2.10 quick sort + specific test case

--- practice/215.kth-largest-element-in-an-array.java
+++ practice/215.kth-largest-element-in-an-array.java
@@ -21,47 +21,80 @@ class Solution {
         // }
         // return maxqQueue.poll();
 
-        heapSort(nums, k);
+        // heapSort(nums, k);
+        // return nums[nums.length - k];
+
+        quicksort(nums, 0, nums.length - 1, k);
         return nums[nums.length - k];
     }
-
-
-    public void heapSort(int[] nums, int k) {
-        // build max heap
-        buildMaxHeap(nums);
-
-        // sort
-        for(int i = 0; i < k; i++) {
-            swap(nums, 0, nums.length - i - 1);
-            heapify(nums, nums.length- i -1, 0);
-         }
-
-    }
-
-    public void buildMaxHeap(int[] nums) {
-        for(int i = nums.length / 2 - 1; i >= 0; i--){
-            heapify(nums, nums.length, i);
-        }
-    }
     
-    public void heapify(int[] nums, int length, int i) {
-        int l = 2 * i + 1;
-        int r = 2 * i + 2;
-
-        int largest = i;
-        if (l < length && nums[l] > nums[largest]) {
-            largest = l;
-        }
-
-        if (r < length && nums[r] > nums[largest]) {
-            largest = r;
-        }
-
-        if (largest != i) {
-            swap(nums, i, largest);
-            heapify(nums, length, largest);
+    public void quicksort(int[] nums, int left, int right, int k) {
+        if (left < right) {
+            int pivot = partition(nums, left, right);
+            if (pivot == nums.length - k) {
+                return;
+            } else if (pivot < nums.length - k) {
+                int lo = pivot + 1;
+                while (lo < k && nums[lo] == nums[lo - 1]) lo++;
+                quicksort(nums, lo, right, k);
+            } else {
+                int hi = pivot - 1;
+                while (hi > nums.length - k && nums[hi] == nums[hi + 1]) hi--;
+                quicksort(nums, left, hi, k);
+            }
         }
     }
+
+    public int partition(int[] nums, int left, int right) {
+        int pivot = nums[right];
+        int i = left;
+        for (int j = left; j < right; j++) {
+            if (nums[j] < pivot) {
+                swap(nums, i, j);
+                i++;
+            }
+        }
+        swap(nums, i, right);
+        return i;
+    }
+
+
+    // public void heapSort(int[] nums, int k) {
+    //     // build max heap
+    //     buildMaxHeap(nums);
+
+    //     // sort
+    //     for(int i = 0; i < k; i++) {
+    //         swap(nums, 0, nums.length - i - 1);
+    //         heapify(nums, nums.length- i -1, 0);
+    //      }
+
+    // }
+
+    // public void buildMaxHeap(int[] nums) {
+    //     for(int i = nums.length / 2 - 1; i >= 0; i--){
+    //         heapify(nums, nums.length, i);
+    //     }
+    // }
+    
+    // public void heapify(int[] nums, int length, int i) {
+    //     int l = 2 * i + 1;
+    //     int r = 2 * i + 2;
+
+    //     int largest = i;
+    //     if (l < length && nums[l] > nums[largest]) {
+    //         largest = l;
+    //     }
+
+    //     if (r < length && nums[r] > nums[largest]) {
+    //         largest = r;
+    //     }
+
+    //     if (largest != i) {
+    //         swap(nums, i, largest);
+    //         heapify(nums, length, largest);
+    //     }
+    // }
     
     public void swap(int[] nums, int left, int right) {
         int i = nums[left];
