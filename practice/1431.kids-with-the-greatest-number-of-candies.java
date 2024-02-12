c9f231db79cc778328ae24622790ecaeb65c9023
jingpeng7527 <pengjing7527@163.com>
2024-02-11 20:46:33 -0800

    2024.2.11 lc75

---
+++ practice/1431.kids-with-the-greatest-number-of-candies.java
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=1431 lang=java
+ *
+ * [1431] Kids With the Greatest Number of Candies
+ */
+
+// @lc code=start
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int max = 0;
+        List<Boolean> res = new ArrayList<>();
+        for (int i = 0; i < candies.length; i++) {
+            max = Math.max(max, candies[i]);
+        }
+
+        for (int i = 0; i < candies.length; i++) {
+            res.add(max - candies[i] <= extraCandies);
+        }
+
+        return res;
+        
+    }
+}
+// @lc code=end
+
