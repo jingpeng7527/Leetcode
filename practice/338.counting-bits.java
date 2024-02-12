37b33ce1805c427555db77f2bf68a23ecba00799
jingpeng7527 <pengjing7527@163.com>
2024-02-11 19:11:57 -0800

    2024.2.11 bit manipulation

---
+++ practice/338.counting-bits.java
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=338 lang=java
+ *
+ * [338] Counting Bits
+ */
+
+// @lc code=start
+class Solution {
+    public int[] countBits(int n) {
+        int[] res = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            // res[i] = Integer.bitCount(i);
+            // res[i] = res[i & (i - 1)] + 1;
+            res[i] = res[i >> 1] + (i & 1);
+        }
+        return res;
+    }
+}
+// @lc code=end
+
