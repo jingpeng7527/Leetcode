da9fcff4b7dc8f0ec8363cc68cbb241d08b41045
jingpeng7527 <pengjing7527@163.com>
2024-02-11 19:15:35 -0800

    2024.2.11 bit manipulation

--- practice/338.counting-bits.java
+++ practice/338.counting-bits.java
@@ -10,7 +10,13 @@ class Solution {
         int[] res = new int[n + 1];
         for (int i = 1; i <= n; i++) {
             // res[i] = Integer.bitCount(i);
+
+            // clear the least significant bit
+            // plus 1
             // res[i] = res[i & (i - 1)] + 1;
+
+            // right shift 1 bit
+            // plus one if the least significant bit is 1
             res[i] = res[i >> 1] + (i & 1);
         }
         return res;
