fbd5f5f7b96654a8928db455ff97b876758df5e6
jingpeng7527 <pengjing7527@163.com>
2024-02-11 16:15:03 -0800

    2024.2.11 merge sort

--- practice/88.merge-sorted-array.java
+++ practice/88.merge-sorted-array.java
@@ -7,22 +7,36 @@
 // @lc code=start
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i = m-1;
-        int j = n-1;
-        int y = m+n-1;
+        int i = m - 1;
+        int j = n - 1;
+        int y = m + n - 1;
+
+        // while (i >= 0 && j >= 0) {
+        //     nums1[y] = Math.max(nums1[i], nums2[j]);
+        //     if (nums1[i] > nums2[j]) {
+        //         i--;
+        //     } else {
+        //         j--;
+        //     }
+        //     y--;
+        // }
+
+        // while (j >= 0) {
+        //     nums1[y--] = nums2[j--];
+        // }
+
         while (i >= 0 && j >= 0) {
-            nums1[y--] = nums1[i] > nums2[j] ? nums1[i--] : nums2[j--];
-            // if (nums1[i] > nums2[j]) {
-            //     nums1[y] = nums1[i];
-            //     i--;
-            // } else {
-            //     nums1[y] = nums2[j];
-            //     j--;
-            // }
-            // y--;
+        nums1[y--] = nums1[i] > nums2[j] ? nums1[i--] : nums2[j--];
+        // if (nums1[i] > nums2[j]) {
+        // nums1[y] = nums1[i];
+        // i--;
+        // } else {
+        // nums1[y] = nums2[j];
+        // j--;
+        // }
+        // y--;
         }
         System.arraycopy(nums2, 0, nums1, 0, j + 1);
     }
 }
 // @lc code=end
-
