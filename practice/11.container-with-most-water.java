328adcc6a35cf33fb91af27ee199e99f813c8e8a
jingpeng7527 <pengjing7527@163.com>
2024-02-18 21:40:09 -0800

    2024.2.18 lc75 + two pointer

--- practice/11.container-with-most-water.java
+++ practice/11.container-with-most-water.java
@@ -9,13 +9,26 @@ class Solution {
     public int maxArea(int[] height) {
         int left = 0;
         int right = height.length - 1;
-        int res = 0;
+        int res = Math.min(height[left], height[right]) * right;
 
         while (left < right) {
-            res = height[left] < height[right]
-                    ? Math.max(res, (right - left) * height[left++])
-                    : Math.max(res, (right - left) * height[right--]);
+            if (height[left]<height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+            res = Math.max(res, Math.min(height[left], height[right]) * (right - left));
         }
+
+        // int left = 0;
+        // int right = height.length - 1;
+        // int res = 0;
+
+        // while (left < right) {
+        //     res = height[left] < height[right]
+        //             ? Math.max(res, (right - left) * height[left++])
+        //             : Math.max(res, (right - left) * height[right--]);
+        // }
 
         // while (left < right) {
         // int area = (right - left) * Math.min(height[left], height[right]);
