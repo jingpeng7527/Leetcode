b6501d5f0224d9c110b67e22e04863bf7ee044a6
jingpeng7527 <pengjing7527@163.com>
2024-02-18 20:57:06 -0800

    2024.2.18 lc75 + slow/fast pointer

--- practice/283.move-zeroes.java
+++ practice/283.move-zeroes.java
@@ -9,19 +9,37 @@ class Solution {
     public void moveZeroes(int[] nums) {
         int slow = 0;
         int fast = 0;
+
         while (fast < nums.length) {
             if (nums[fast] != 0) {
-                nums[slow] = nums[fast];
-                slow++;
+                nums[slow++] = nums[fast];
+
             }
             fast++;
         }
-
-        // System.out.println(slow+"  "+fast);
         
-        while (slow<nums.length) {
+        while (slow <nums.length) {
             nums[slow++] = 0;
         }
+
+
+
+
+        // int slow = 0;
+        // int fast = 0;
+        // while (fast < nums.length) {
+        //     if (nums[fast] != 0) {
+        //         nums[slow] = nums[fast];
+        //         slow++;
+        //     }
+        //     fast++;
+        // }
+
+        // // System.out.println(slow+"  "+fast);
+        
+        // while (slow<nums.length) {
+        //     nums[slow++] = 0;
+        // }
     }
 }
 // @lc code=end
