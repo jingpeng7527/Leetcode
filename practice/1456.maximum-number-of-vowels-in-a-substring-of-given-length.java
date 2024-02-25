f327a91ba59e4d201af61d308ce7525fc6ccc5e0
jingpeng7527 <pengjing7527@163.com>
2024-02-25 13:03:23 -0800

    2024.2.23 lc75 + sliding window

--- practice/1456.maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ practice/1456.maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -7,26 +7,60 @@
 // @lc code=start
 class Solution {
     public int maxVowels(String s, int k) {
+        // int left = 0;
+        int right = k;
         int res = 0;
 
         for (int i = 0; i < k; i++) {
-            res += isVowel(s.charAt(i));
-            // System.out.println(res+"  kk ");
+            if (isVowel(s.charAt(i))) {
+                res++;
+            }
         }
-        int vowel_count = res;
-        for (int i = k; i < s.length(); ++i) {
-            vowel_count += isVowel(s.charAt(i)) - isVowel(s.charAt(i - k));
-            res = Math.max(res, vowel_count);
+
+        // System.out.println(res);
+        int cur = res;
+
+        while (right < s.length()) {
+            if (isVowel(s.charAt(right - k))) {
+                // System.out.println(s.charAt(right-k)+"---");
+                cur--;
+            }
+            if (isVowel(s.charAt(right))) {
+                // System.out.println(s.charAt(right));
+
+                cur++;
+            }
+            
+            right++;
+            // System.out.println(cur+"ll");         
+            res = Math.max(res, cur);
         }
-        
-        
+
         return res;
+
+        //     int res = 0;
+
+        //     for (int i = 0; i < k; i++) {
+        //         res += isVowel(s.charAt(i));
+        //         // System.out.println(res+"  kk ");
+        //     }
+        //     int vowel_count = res;
+        //     for (int i = k; i < s.length(); ++i) {
+        //         vowel_count += isVowel(s.charAt(i)) - isVowel(s.charAt(i - k));
+        //         res = Math.max(res, vowel_count);
+        //     }
+
+        //     return res;
 
     }
     
-    public int isVowel(char ch) {
-        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ? 1 : 0;
+    public boolean isVowel(char ch) {
+        return "aeuio".indexOf(ch)>=0;
     }
+    
+    // public int isVowel(char ch) {
+    //     return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ? 1 : 0;
+    // }
 }
 // @lc code=end
 
