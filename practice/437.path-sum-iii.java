8bcad838a4e53b3d48ee48ff4b475126d5568375
jingpeng7527 <pengjing7527@163.com>
2024-03-19 01:26:02 -0700

    2024.3.18 lc75 + presum + dfs

--- practice/437.path-sum-iii.java
+++ practice/437.path-sum-iii.java
@@ -22,27 +22,52 @@
  */
 class Solution {
     int res = 0;
+    Map<Long, Integer> map = new HashMap<>();
+
     public int pathSum(TreeNode root, int targetSum) {
-        if (root == null)
-            return 0;
-        dfs(root, targetSum - root.val);
-        pathSum(root.left, targetSum);
-        pathSum(root.right, targetSum);
+        if (root == null) return 0;
+        map.put(0L, 1);
+        dfs(root, targetSum, root.val);
         return res;
     }
 
-    public void dfs(TreeNode node, long targetSum) {
-        if (targetSum == 0) {
-            res++;
+    public void dfs(TreeNode node, int targetSum, long curSum) {
+        if (map.containsKey(curSum - targetSum)) {
+            res += map.get(curSum - targetSum);
         }
+        map.put(curSum, map.getOrDefault(curSum, 0)+1);
 
         if (node.left != null) {
-            dfs(node.left, targetSum - node.left.val);
+            dfs(node.left, targetSum, curSum+node.left.val);
         }
         if (node.right != null) {
-            dfs(node.right, targetSum - node.right.val);
+            dfs(node.right, targetSum, curSum+node.right.val);
         }
+
+        map.put(curSum, map.getOrDefault(curSum, 0)-1);
     }
+    // int res = 0;
+    // public int pathSum(TreeNode root, int targetSum) {
+    //     if (root == null)
+    //         return 0;
+    //     dfs(root, targetSum - root.val);
+    //     pathSum(root.left, targetSum);
+    //     pathSum(root.right, targetSum);
+    //     return res;
+    // }
+
+    // public void dfs(TreeNode node, long targetSum) {
+    //     if (targetSum == 0) {
+    //         res++;
+    //     }
+
+    //     if (node.left != null) {
+    //         dfs(node.left, targetSum - node.left.val);
+    //     }
+    //     if (node.right != null) {
+    //         dfs(node.right, targetSum - node.right.val);
+    //     }
+    // }
 }
 // @lc code=end
 
