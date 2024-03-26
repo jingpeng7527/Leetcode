1d3485298a94fa3e595a19614f1d762617e204e4
jingpeng7527 <pengjing7527@163.com>
2024-03-25 19:37:29 -0700

    2024.3.25 lc75 + bfs(dfs)

--- practice/199.binary-tree-right-side-view.java
+++ practice/199.binary-tree-right-side-view.java
@@ -28,32 +28,80 @@ import javax.swing.tree.TreeNode;
  * }
  */
 class Solution {
+    List<Integer> res = new LinkedList<>();
+
     public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> list = new ArrayList<>();
-        Queue<TreeNode> que = new LinkedList<>();
-        if (root != null) {
-            que.add(root);
-        }
+        dfs(root, 0);
 
-        while (!que.isEmpty()) {
-            int len = que.size();
+        // Queue<TreeNode> queue = new LinkedList<>();
+        // List<Integer> res = new LinkedList<>();
 
-            while (len > 0) {
-                TreeNode node = que.poll();
-                if (node.left != null) {
-                    que.add(node.left);
-                }
-                if (node.right != null) {
-                    que.add(node.right);
-                }
-                if (len == 1) {
-                    list.add(node.val);
-                }
-                len--;
-            }
+        // int n = 0;
+        // if (root == null) {
+        // return res;
+        // } else {
+        // n = 1;
+        // queue.offer(root);
+        // }
 
-        }
-        return list;
+        // while (!queue.isEmpty()) {
+        // int len = n;
+        // n = 0;
+        // for (int i = 0; i < len; i++) {
+        // TreeNode cur = queue.poll();
+        // if (cur.left != null) {
+        // queue.offer(cur.left);
+        // n++;
+        // }
+        // if (cur.right != null) {
+        // queue.offer(cur.right);
+        // n++;
+        // }
+        // if (i == len - 1) {
+        // res.add(cur.val);
+        // }
+        // }
+        // }
+        return res;
     }
+
+    public void dfs(TreeNode root, int depth) {
+        if (root == null) {
+            return;
+        }
+
+        if (depth == res.size())
+            res.add(root.val);
+
+        dfs(root.right, depth + 1);
+        dfs(root.left, depth + 1);
+    }
+    // public List<Integer> rightSideView(TreeNode root) {
+    //     List<Integer> list = new ArrayList<>();
+    //     Queue<TreeNode> que = new LinkedList<>();
+    //     if (root != null) {
+    //         que.add(root);
+    //     }
+
+    //     while (!que.isEmpty()) {
+    //         int len = que.size();
+
+    //         while (len > 0) {
+    //             TreeNode node = que.poll();
+    //             if (node.left != null) {
+    //                 que.add(node.left);
+    //             }
+    //             if (node.right != null) {
+    //                 que.add(node.right);
+    //             }
+    //             if (len == 1) {
+    //                 list.add(node.val);
+    //             }
+    //             len--;
+    //         }
+
+    //     }
+    //     return list;
+    // }
 }
 // @lc code=end
