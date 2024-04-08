2e4cc5787e4b0356da7a540b06ed03f88b5a4647
jingpeng7527 <pengjing7527@163.com>
2024-04-07 19:23:09 -0700

    2024.4.7 lc75 + bfs

--- practice/547.number-of-provinces.java
+++ practice/547.number-of-provinces.java
@@ -6,10 +6,36 @@
 
 // @lc code=start
 class Solution {
+    List<Integer> visited = new LinkedList<>();
     public int findCircleNum(int[][] isConnected) {
-        
-        
+        int res = 0;
+        for (int i = 0; i < isConnected.length; i++) {
+            if (!visited.contains(i)) {
+                bfs(isConnected, i);
+                res++;
+            }
+        }
+        return res;
     }
-}
-// @lc code=end
 
+    public void bfs(int[][] isConnected, int start) {
+        Queue<Integer> que = new LinkedList<>();
+        que.offer(start);
+        int layer = start;
+
+        while (!que.isEmpty()) {
+            layer = que.poll();
+
+            if (visited.contains(layer)) {
+                continue;
+            }
+            visited.add(layer);
+            for (int i = 0; i < isConnected.length; i++) {
+                if (isConnected[layer][i] == 1 && !visited.contains(i)) {
+                    que.add(i);
+                }
+            }
+        }
+    }
+
+}
